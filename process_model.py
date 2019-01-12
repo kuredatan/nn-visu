462f457766628fdb5964eb8cc69a388d9331bf11
Clémence Réda <reda.clmc@live.fr>
2019-01-12 16:25:51 +0100

    code

--- process_model.py
+++ process_model.py
@@ -73,7 +73,7 @@ parser.add_argument('--verbose', type=int, default=0, metavar='V',
                     help='Whether to print things or not: in {0, 1}.')
 parser.add_argument('--subtask', type=str, default="", metavar='S',
                     help='Sub-task for deconvolution.')
-parser.add_argument('--tdeconv', type=str, default="keras", metavar='K',
+parser.add_argument('--tdeconv', type=str, default="custom", metavar='K',
                     help='Choice of implementation for deconvolution: Mihai Dusmanu\'s (\'custom\') or DeepLearningImplementations (\'keras\').')
 parser.add_argument('--loss', type=str, default="categorical_crossentropy", metavar='O',
                     help='Choice of loss function, among those supported by Keras.')
@@ -265,8 +265,15 @@ def process_fmap(out, im, layer="", sz=sz):
 	plt.axis('off')
 	plt.xlabel("Input image")
 	plt.show()
+
+def save_fmap(out, layer="", sz=sz):
+	layer = "_"+layer
+	out = np.resize(out, (sz, sz, 3))
+	plt.imshow(out)
+	plt.axis('off')
+	plt.title("Feature map for layer " + layer[1:])
 	if (query_yes_no("Save feature map?", default="yes")):
-		plt.savefig(out, "feature_map_layer" + layer + ".png", bbox="tight")
+		plt.savefig("feature_map_layer" + layer + ".png", bbox_inches="tight")
 
 ## Generator for training data
 datagen_train = ImageDataGenerator(
@@ -303,109 +310,23 @@ if (args.trun == "testing"):
 	Y_test = Y_test[:k]
 	labels = run_nn(datagen_test, X_test, Y_test_c, Y_test, args.batch, training=False, verbose=True)
 if (args.trun == "deconv"):
-	im = X_test[0, :, :, :]
-	im = np.expand_dims(im, axis=0)
-	im = preprocess_image(im)
-	plt.imshow(np.resize(im, np.shape(im)[1:])*255.)
-	plt.show()
-	print(imagenet1000[Y_test[0]])
+	im_nb = 0
+	layer_nb = 5
+	layer_name = model.layers[layer_nb].name
+	im = preprocess_image(np.expand_dims(X_test[im_nb, :, :, :], axis=0))
 	out = model.predict([im])
-	print(len(out))
-	print(list(map(np.shape, out)))
+	## Images
 	out = deconv_model.predict(out)
-	process_fmap(out, im)
-	raise ValueError
-	if (args.tdeconv == "keras"):
-		layer_name = layers[-2]
-		i = 0
-		print("* Target layer: \'" + layer_name + "\' and image #" + str(i+1))
-		out = deconv_model.get_deconv(np.reshape(X_test[i, ::], (1, sz, sz, 3)), layer_name) # target_layer
+	if (args.verbose):
+		process_fmap(out, im, layer=layer_name)
+	save_fmap(out, layer=layer_name)
+	## Weights
+	weight = model.layers[layer_nb].get_weights()
+	if (len(weight) > 0):
+		for i in range(len(weight)):
+			weight = weight[i]*255.
+			if (args.verbose):
+				process_fmap(weight, im, layer="weight" + str(i) + "_"+layer_name)
+			save_fmap(weight, layer="weight" + str(i) + "_"+layer_name)
 	else:
-		layer_name = layers[-2]
-		out = deconv_model.predict(out) # layer=layer_name
-	plt.figure(figsize=(20, 20))
-	plt.imshow(out)
-	plt.show()
-	raise ValueError
-	## Save output feature map
-	#If you want to reconstruct from a single feature map / activation, you can
-	# simply set all the others to 0. (in file "models/deconv_models.py")
-	plt.savefig(folder + "fmap_" + str(i) + ".png", bbox_inches="tight")
-	if (args.subtask == "max_activation"):
-		get_max_act = True
-		if get_max_act:
-			if not model:
-				model = load_model('./Data/vgg16_weights.h5')
-			if not Dec:
-				Dec = KerasDeconv.DeconvNet(model)
-		d_act_path = './Data/dict_top9_mean_act.pickle'
-		d_act = {"convolution2d_13": {},
-			 "convolution2d_10": {}
-			 }
-		for feat_map in range(10):
-			d_act["convolution2d_13"][feat_map] = find_top9_mean_act(
-				data, Dec, "convolution2d_13", feat_map, batch_size=32)
-			d_act["convolution2d_10"][feat_map] = find_top9_mean_act(
-				data, Dec, "convolution2d_10", feat_map, batch_size=32)
-			with open(d_act_path, 'w') as f:
-				pickle.dump(d_act, f)
-
-
-###################################################################
-###################################################################
-
-#bow_comparison(fmap, images_list, list_img=list_img)
-#corresp_comparison(fmap, images_list, list_img=list_img)
-#repeatability_harris(fmap, images_list, list_img=list_img)
-
-    ###############################################
-    # Action 1) Get max activation for a secp ~/deconv_specificlection of feat maps
-    ###############################################
-
-
-    ###############################################
-    # Action 2) Get deconv images of images that maximally activate
-    # the feat maps selected in the step above
-    ###############################################
-#    deconv_img = True
-#    if deconv_img:
-#        d_act_path = './Data/dict_top9_mean_act.pickle'
-#        d_deconv_path = './Data/dict_top9_deconv.pickle'
-#        if not model:
-#            model = load_model('./Data/vgg16_weights.h5')
-#        if not Dec:
-#            Dec = KerasDeconv.DeconvNet(model)
-#        get_deconv_images(d_act_path, d_deconv_path, data, Dec)
-
-    ###############################################
-    # Action 3) Get deconv images of images that maximally activate
-    # the feat maps selected in the step above
-    ###############################################
-#    plot_deconv_img = True
-#    if plot_deconv_img:
-#        d_act_path = './Data/dict_top9_mean_act.pickle'
-#        d_deconv_path = './Data/dict_top9_deconv.npz'
-#        target_layer = "convolution2d_10"
-#        plot_max_activation(d_act_path, d_deconv_path,
-#                            data, target_layer, save=True)
-#
-    ###############################################
-    # Action 4) Get deconv images of some images for some
-    # feat map
-    ###############################################
-#    deconv_specific = False
-#    img_choice = False  # for debugging purposes
-#    if deconv_specific:
-#        if not model:
-#            model = load_model('./Data/vgg16_weights.h5')
-#        if not Dec:
-#            Dec = KerasDeconv.DeconvNet(model)
-#        target_layer = "convolution2d_13"
-#        feat_map = 12
-#        num_img = 25
-#        if img_choice:
-#            img_index = []
-#            assert(len(img_index) == num_img)
-#        else:
-#            img_index = np.random.choice(data.shape[0], num_img, replace=False)
-#        plot_deconv(img_index, data, Dec, target_layer, feat_map)
+		print("Layer " + layer_name + " has no weights!")
